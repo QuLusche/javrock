8f49b0ac1bedbe3b7ede0d65c611741e6b6ede6d
QuLusche <quenquenl.jeux@gmail.com>
2026-03-16 17:30:47 +0100

    Add dependencies

--- src/main/java/fr/qulusche/javrock/Javrock.java
+++ src/main/java/fr/qulusche/javrock/Javrock.java
@@ -1,17 +1,28 @@
 package fr.qulusche.javrock;
 
+import com.tcoded.folialib.FoliaLib;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Javrock extends JavaPlugin {
 
+	public static JavaPlugin instance;
+
+	private static FoliaLib foliaLib;
+
 	@Override
 	public void onEnable() {
-		// Plugin startup logic
+		instance = this;
 
+		foliaLib = new FoliaLib(this);
+
+		getLogger().info("Javrock has been enabled!");
 	}
 
 	@Override
 	public void onDisable() {
-		// Plugin shutdown logic
+		foliaLib.getScheduler().cancelAllTasks();
+
+		instance = null;
+		getLogger().info("Javrock has been disabled!");
 	}
 }
